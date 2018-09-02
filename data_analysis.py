aa0c1646d3484e2dbfda58b3917270251e6e1f66
joecode <joseph.marchant@satavia.com>
2018-09-02 19:21:44 +0100

    After testing many different types of min/max of a list functions, found simple min(list) and max(list) to be fastest in almost all cases. Created a function to use this, returning a range dict of columns of a database

--- data_analysis.py
+++ data_analysis.py
@@ -6,27 +6,13 @@ import numpy as np
 # TODO: etc.
 
 
-def merge_to_compare_columns(df1, df2, merge_columns, compare_columns):
-    """This function merges two dataframes by some common columns, and removes
-    unwanted ones, so we can easily compare/ work with some select columns
+def return_value_ranges(df_data, value_columns, many_duplicates=False):
+    ranges_dict = dict()
 
-    Args:
-        df1 (pandas.DataFrame): First of two DataFrames that we merge
-        df2 (pandas.DataFrame): Second of two DataFrames that we merge (order does not matter)
-        merge_columns (String list): Header names for columns we want to merge on
-        compare_columns (String list): Header names for columns we wish to compare
+    for col in list(df_data.columns.values):
+        col_list = df_data[col].tolist()
+        col_min = min(col_list)
+        col_max = max(col_list)
+        ranges_dict[col] = [col_min, col_max]
 
-    Returns:
-        df: Merged DataFrame with only the columns we want to compare, and the merging columns
-    """
-
-    df = df1.merge(df2, how='inner', on=merge_columns)
-    new_cols = [x + "_x" for x in compare_columns]
-    new_cols.extend([x + "_y" for x in compare_columns])
-    new_cols.extend(merge_columns)
-
-    remove_cols = [x for x in list(df.columns.values) if not x in new_cols]
-
-    df.drop(remove_cols, axis=1, inplace=True)
-
-    return df
+    return ranges_dict
